66996bdf7400786a034ee2caa746462bfa643001
James Pascoe <james.pascoe@bluwireless.com>
2022-03-11 18:13:47 +0000

    Add co_chat Example

--- co_chat/co_chat.cpp
+++ co_chat/co_chat.cpp
@@ -2,117 +2,141 @@
 // co_chat.cpp
 // ~~~~~~~~~~~
 //
-// g++-11 -I/usr/local/boost_1_78_0/include -fcoroutines -std=c++20 -Wall -Werror co_receive.cpp -l pthread
-// ./a.out 127.0.0.1 6666 127.0.0.1 6667
+// g++-11 -I/usr/local/boost_1_78_0/include -fcoroutines -std=c++20 -Wall -Werror -o co_chat co_chat.cpp -l pthread
 //
+// terminal 1> ./co_chat 127.0.0.1 6666 127.0.0.1 7777
+// terminal 2> ./co_chat 127.0.0.1 7777 127.0.0.1 6666
 
 #include <boost/asio.hpp>
-#include <boost/regex.hpp>
 #include <boost/asio/experimental/as_tuple.hpp>
-#include <boost/asio/experimental/awaitable_operators.hpp>
-#include <boost/asio/experimental/channel.hpp>
+#include <boost/regex.hpp>
+
 #include <iostream>
 
 using boost::asio::awaitable;
 using boost::asio::buffer;
-using boost::asio::dynamic_buffer;
 using boost::asio::co_spawn;
-using boost::system::error_code;
 using boost::asio::detached;
-using boost::asio::experimental::as_tuple;
-using boost::asio::experimental::channel;
+using boost::asio::dynamic_buffer;
 using boost::asio::io_context;
-using boost::asio::ip::tcp;
 using boost::asio::steady_timer;
 using boost::asio::use_awaitable;
-namespace this_coro = boost::asio::this_coro;
-using namespace boost::asio::experimental::awaitable_operators;
-using namespace std::literals::chrono_literals;
+using boost::asio::ip::tcp;
+using boost::asio::ip::basic_resolver_entry;
+using boost::asio::experimental::as_tuple;
 
-awaitable<void> transmit(tcp::socket remote_sock, tcp::endpoint remote)
-{
-  for (;;) {
-    auto [e] = co_await remote_sock.async_connect(remote, as_tuple(use_awaitable));
-    if (!e)
-    {
-      std::cout << "Connected to: " << remote << std::endl;
-      break;
-    }
+using boost::system::error_code;
+
+class chat {
+
+public:
+
+  chat(char *addr, char *port, char *remote_addr, char *remote_port)
+      : remote_sock(ctx) {
+
+    tcp::resolver resolver(ctx);
+    basic_resolver_entry<tcp> listen_endpoint;
+    basic_resolver_entry<tcp> remote_endpoint;
+
+    listen_endpoint = *resolver.resolve(addr, port, tcp::resolver::passive);
+    remote_endpoint = *resolver.resolve(remote_addr, remote_port);
+
+    co_spawn(ctx, transmit(remote_endpoint), detached);
+    co_spawn(ctx, receive(listen_endpoint), detached);
+
+    ctx.run();
   }
 
-  std::string data;
-  for (;;)
-  {
-    struct pollfd input[1] = {{.fd = 0, .events = POLLIN}};
-    if (poll(input, 1, 100)) {
-      char c;
-      while (std::cin.get(c) && c != '\n')
-        data += c;
+private:
 
-      data += "\r\n";
-    }
+  awaitable<void> transmit(tcp::endpoint remote) {
 
-    co_await async_write(remote_sock, buffer(data), as_tuple(use_awaitable));
+    for (;;) {
 
-    data.clear();
-  }
-}
+      auto [error] =
+          co_await remote_sock.async_connect(remote, as_tuple(use_awaitable));
+      if (!error) {
+        std::cout << "Connected to: " << remote << std::endl;
+        connected = true;
+      } else {
+        std::cout << "Could not connect to: " << remote << " - retrying in 1s"
+                  << std::endl;
 
-awaitable<void> receive(io_context& ctx, tcp::endpoint listen)
-{
-  tcp::acceptor acceptor(ctx, listen);
-  for (;;)
-  {
-    auto [e, client] = co_await acceptor.async_accept(as_tuple(use_awaitable));
-    if (!e)
-    {
-      std::string str;
+        steady_timer timer(co_await boost::asio::this_coro::executor);
+        timer.expires_after(std::chrono::seconds(1));
+        co_await timer.async_wait(use_awaitable);
+        continue;
+      }
 
-      // TODO - do this until the client disconnects
-      for (;;) {
-        co_await async_read_until(client, dynamic_buffer(str), boost::regex("\r\n"), use_awaitable);
+      std::string data;
+      while (connected) {
+        struct pollfd input[1] = {{.fd = 0, .events = POLLIN}};
+        if (poll(input, 1, 100)) {
+          char c;
+          while (std::cin.get(c) && c != '\n')
+            data += c;
 
-        std::cout << client.remote_endpoint() << "> " << str << std::endl;
-        str.clear();
+          data += "\r\n";
+        }
+
+        co_await async_write(remote_sock, buffer(data),
+                             as_tuple(use_awaitable));
+
+        data.clear();
       }
     }
-    else
-    {
-      std::cerr << "Accept failed: " << e.message() << "\n";
-      steady_timer timer(co_await this_coro::executor);
-      timer.expires_after(100ms);
-      co_await timer.async_wait(use_awaitable);
+  }
+
+  awaitable<void> receive(tcp::endpoint listen) {
+
+    tcp::acceptor acceptor(ctx, listen);
+
+    for (;;) {
+      auto [error, client] =
+          co_await acceptor.async_accept(as_tuple(use_awaitable));
+
+      if (!error) {
+        std::string str;
+
+        for (;;) {
+          auto [error, len] = co_await async_read_until(
+              client, dynamic_buffer(str), boost::regex("\r\n"),
+              as_tuple(use_awaitable));
+
+          if (error == boost::asio::error::eof) { // remote has disconnected
+            remote_sock.close();
+            connected = false;
+            break;
+          }
+
+          std::cout << client.remote_endpoint() << "> " << str;
+          str.clear();
+        }
+      } else {
+        std::cerr << "Accept failed: " << error.message() << "\n";
+      }
     }
   }
-}
 
-int main(int argc, char* argv[])
-{
-  try
-  {
-    if (argc != 5)
-    {
-      std::cerr << "Usage: co_receive";
+  // Member variables (shared between coroutines)
+  io_context ctx;
+  tcp::socket remote_sock;
+  bool connected = false;
+};
+
+int main(int argc, char *argv[]) {
+
+  try {
+    if (argc != 5) {
+      std::cerr << "Usage: " << argv[0];
       std::cerr << " <listen_address> <listen_port>";
       std::cerr << " <remote_address> <remote_port>\n";
       return 1;
     }
 
-    io_context ctx;
+    chat(argv[1], argv[2], argv[3], argv[4]);
 
-    auto listen_endpoint =
-      *tcp::resolver(ctx).resolve(argv[1], argv[2], tcp::resolver::passive);
-
-    auto remote_endpoint =
-      *tcp::resolver(ctx).resolve(argv[3], argv[4]);
-
-    tcp::socket remote_sock(ctx);
-    co_spawn(ctx, transmit(std::move(remote_sock), remote_endpoint), detached);
-    co_spawn(ctx, receive(ctx, listen_endpoint), detached);
-    ctx.run();
-  }
-  catch (std::exception& e)
-  {
+  } catch (std::exception &e) {
     std::cerr << "Exception: " << e.what() << "\n";
   }
 }
