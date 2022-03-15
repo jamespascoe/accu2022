b25e93e83a6d6cce8d3f009dc339c0ca5b187c1b
James Pascoe <james.pascoe@bluwireless.com>
2022-03-15 22:54:09 +0000

    Update echo server examples

--- echo_server/echo_server_coroutine.cpp
+++ echo_server/echo_server_coroutine.cpp
@@ -2,8 +2,8 @@
 // echo_server_coroutine.cpp
 // -------------------------
 //
-// g++-11 -I/usr/local/boost_1_78_0/include -fcoroutines -std=c++20 -Wall
-// -Werror echo_server_coroutine.cpp -o coroutine -l pthread
+// g++-11 -I/usr/local/boost_1_78_0/include -fcoroutines -std=c++20
+// -Wall -Werror echo_server_coroutine.cpp -o coroutine -l pthread
 //
 
 #include <boost/asio.hpp>
@@ -11,12 +11,12 @@
 
 #include <iostream>
 
-using boost::asio::io_context;
-using boost::asio::ip::tcp;
 using boost::asio::buffer;
+using boost::asio::detached;
+using boost::asio::io_context;
 using boost::asio::use_awaitable;
 using boost::asio::experimental::as_tuple;
-using boost::asio::detached;
+using boost::asio::ip::tcp;
 using boost::system::error_code;
 
 boost::asio::awaitable<void> echo(tcp::socket peer_socket,
@@ -32,7 +32,8 @@ boost::asio::awaitable<void> echo(tcp::socket peer_socket,
       if (error == boost::asio::error::eof)
         break;
 
-      co_await async_write(peer_socket, buffer(buf, len), use_awaitable);
+      co_await async_write(peer_socket, buffer(buf, len),
+                           use_awaitable);
     }
 
     peer_socket.close();
@@ -46,7 +47,8 @@ int main() {
     tcp::socket socket(ctx);
     tcp::acceptor acceptor(ctx, tcp::endpoint(tcp::v4(), 6666));
 
-    co_spawn(ctx, echo(std::move(socket), std::move(acceptor)), detached);
+    co_spawn(ctx, echo(std::move(socket), std::move(acceptor)),
+             detached);
 
     ctx.run();
   } catch (std::exception &e) {
